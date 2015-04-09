30aadd6576b0134393125df4dcb72ad4d6a2e69a
Andrea Curo <andreanicole@utexas.edu>
2015-04-09 11:28:02 -0500

    started crosstab

--- 01 SQL Crosstabs/data.R
+++ 01 SQL Crosstabs/data.R
@@ -1,10 +1,20 @@
-df <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query="select * from titanic "')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_THC359', PASS='orcl_THC359', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)));
+df <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query="select * from titanic"')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_THC359', PASS='orcl_THC359', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)));
 
 tbl_df(df)
 
-
+#rank fare paid within an age group
 df <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query=
-"SELECT sex, age, , rank() 
-OVER (PARTITION BY deptno order by sal desc) DEPT_RANK 
-FROM emp order by 2,3 desc"
-')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDB1.usuniversi01134.oraclecloud.internal', USER='DV_Vehicles', PASS='orcl', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE))); tbl_df(df)
+"SELECT name, sex, age, fare rank() 
+OVER (PARTITION BY age order by fare desc) AGE_RANK 
+FROM titanic order by 2,3 desc"
+')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_THC359', PASS='orcl_THC359', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)));
+
+tbl_df(df)
+
+#last value
+
+
+#nth_value
+
+
+#cume_dist
