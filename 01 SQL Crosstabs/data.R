cd7a1befec5c41bfd588c915617f0c7009948dda
Andrea Curo <andreanicole@utexas.edu>
2015-04-11 15:38:18 -0500

    completed first 2 crosstabs

--- 01 SQL Crosstabs/data.R
+++ 01 SQL Crosstabs/data.R
@@ -4,16 +4,25 @@ df <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/
 
 tbl_df(df)
 
-#rank fare paid within an age group
-df1 <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query=
-"SELECT me, age, fare, rank() 
-OVER (PARTITION BY age order by fare desc) AGE_RANK 
-FROM titanicdata"
-')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_THC359', PASS='orcl_THC359', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)));
+#rank fare paid within a class
+df_rank <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query=
+"SELECT me, sex, fare, pclass, rank() 
+OVER (PARTITION BY pclass order by fare desc) FARE_RANK
+FROM titanicdata order by 2,3 desc"
+')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_anc2275', PASS='orcl_anc2275', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE))); tbl_df(df_rank)
 
-tbl_df(df1)
 
-#last value
+#Use last value to find the max fare in a class and the difference between the max
+df_last <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query=
+"select me, pclass, fare, last_value(max_fare) 
+OVER (PARTITION BY pclass order by fare) max_fare, last_value(max_fare) 
+OVER (PARTITION BY pclass order by fare) - fare fare_diff
+from
+(SELECT me, pclass, fare, max(fare)
+OVER (PARTITION BY pclass) max_fare
+FROM titanicdata) 
+order by 2,3 desc"
+')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_anc2275', PASS='orcl_anc2275', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE))); tbl_df(df_last)
 
 
 #nth_value
